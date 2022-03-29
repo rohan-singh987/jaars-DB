b9f6053a8798373aefc7a1e2894bcd206ede4fea
Rohan Singh <rohannsingh98765@gmail.com>
2022-03-29 10:41:30 +0530

    🥸
    Signed-off-by: Rohan Singh <rohannsingh98765@gmail.com>

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"sync"
 
 	"github.com/jcelliott/lumber"
+
 )
 
 const Version = "1.0.0"
@@ -148,8 +149,24 @@ func  (d *Driver) ReadAll(collection string)([]string, error){
 	return records, nil
 }
 
-func  (d *Driver) Delete() error{
+func  (d *Driver) Delete(collection, resource string) error{
+	path := filepath.Join(collection, resource)
+	mutex := d.getOrCreateMutex(collection)
+	mutex.Lock()
+	defer mutex.Unlock()
 
+	dir := filepath.Join(d.dir, path)
+
+	switch fi, err := stat(dir);{
+	case fi == nil, err != nil:
+		return fmt.Errorf("unable to find file or directory %v\n", path)
+
+	case fi.Mode().IsDir():
+		return os.RemoveAll(dir)
+	case fi.Mode().IsRegular():
+		return os.RemoveAll(dir + ".json")
+	}
+	return nil
 }
 
 func (d *Driver) getOrCreateMutex(collection string) *sync.Mutex{
